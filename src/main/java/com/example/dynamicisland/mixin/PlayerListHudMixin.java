b2bed97f29c690c13ed5d4cf66b941c34fc068bd
funku91 <3982743997@qq.com>
2026-09-19 13:57:29 +0800

    Change mixin target from PlayerListHud to PlayerTabOverlay

--- src/main/java/com/example/dynamicisland/mixin/PlayerListHudMixin.java
+++ src/main/java/com/example/dynamicisland/mixin/PlayerListHudMixin.java
@@ -1,13 +1,13 @@
 package com.example.dynamicisland.mixin;
 
 import com.example.dynamicisland.DynamicIslandHud;
-import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.client.gui.components.PlayerTabOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerListHud.class)
+@Mixin(PlayerTabOverlay.class)
 public class PlayerListHudMixin {
 
     @Inject(method = "setVisible", at = @At("HEAD"), require = 1)
