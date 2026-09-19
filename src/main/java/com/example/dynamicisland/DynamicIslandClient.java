cafc327efc3209057faea5e514a97f8b372297dd
funku91 <3982743997@qq.com>
2026-09-19 13:56:33 +0800

    Update DynamicIslandClient.java

--- src/main/java/com/example/dynamicisland/DynamicIslandClient.java
+++ src/main/java/com/example/dynamicisland/DynamicIslandClient.java
@@ -3,14 +3,14 @@ package com.example.dynamicisland;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.Identifier;
 
 public class DynamicIslandClient implements ClientModInitializer {
 
     public static final String MOD_ID = "dynamic-island";
 
     public static Identifier id(String path) {
-        return Identifier.of(MOD_ID, path);
+        return Identifier.fromNamespaceAndPath(MOD_ID, path);
     }
 
     @Override
